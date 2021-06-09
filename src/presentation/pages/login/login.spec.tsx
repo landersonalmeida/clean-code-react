3edacb6de070b10d73b9c27177d1d16585103ce2
Landersson Almeida <clandokca@gmail.com>
2021-06-09 19:40:36 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, RenderResult, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Login from './Login'
 
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
+
 describe('Login Component', () => {
   test('Should start with initial value', () => {
-    render(<Login />)
+    makeSut()
+
     const errorWrap = screen.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
 
@@ -13,6 +25,11 @@ describe('Login Component', () => {
     expect(submitButton).toBeDisabled()
 
     const emailStatus = screen.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus).toHaveTextContent('🔴')
+
+    const passwordStatus = screen.getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus).toHaveTextContent('🔴')
   })
 })
