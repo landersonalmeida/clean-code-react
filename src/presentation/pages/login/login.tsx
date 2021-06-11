063b79fe9cdcec383810a3284a991834e856efaf
Landersson Almeida <clandokca@gmail.com>
2021-06-11 15:35:18 -0300

    feat: ensure Login presents valid state if email is valid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -26,13 +26,6 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     })
   }, [state.email, state.password])
 
-  // useEffect(() => {
-  //   setState({
-  //     ...state,
-  //     passwordError: validation.validate('password', state.password)
-  //   })
-  // }, [state.password])
-
   return (
     <div className={Styles.login}>
       <LoginHeader />
