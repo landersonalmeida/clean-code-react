79db88e83df1f6d0fbd3138f4d1f4cbe93d26ace
Landersson Almeida <clandokca@gmail.com>
2021-06-11 22:09:57 -0300

    feat: ensure Login add accessToken to localStorage on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -39,9 +39,14 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
       setState({ ...state, isLoading: true })
 
-      await authentication.auth({ email: state.email, password: state.password })
+      const account = await authentication.auth({ email: state.email, password: state.password })
+      localStorage.setItem('accessToken', account.accessToken)
     } catch (error) {
-      setState({ ...state, isLoading: false, mainError: (error as InvalidCredentialsError).message })
+      setState({
+        ...state,
+        isLoading: false,
+        mainError: (error as InvalidCredentialsError).message
+      })
     }
   }
 
