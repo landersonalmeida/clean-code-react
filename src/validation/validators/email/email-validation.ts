30779e6273d4cb896eca45672afc54e9c18f59a1
Landersson Almeida <clandokca@gmail.com>
2021-06-12 22:42:43 -0300

    refactor: move InvalidFieldError import

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,5 +1,5 @@
-import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/field-validation'
+import { InvalidFieldError } from '@/validation/errors'
 
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
