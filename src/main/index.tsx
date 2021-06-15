45bfb096e8012fd6fafb1bdba08be656ecbf68bc
Landersson Almeida <clandokca@gmail.com>
2021-06-15 16:43:29 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
+import { makeLogin } from '@/main/factories/pages/login/login-factory'
 
 ReactDOM.render(
-  <Router />,
+  <Router makeLogin={makeLogin} />,
   document.getElementById('main')
 )
