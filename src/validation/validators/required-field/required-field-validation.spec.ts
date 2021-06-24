05468d0e2baa450ac6c197e6edac518c2577c0fb
Landersson Almeida <clandokca@gmail.com>
2021-06-24 17:37:33 -0300

    fix: RequiredFieldValidation test name

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -5,7 +5,7 @@ import faker from 'faker'
 const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
 
 describe('RequiredFieldValidation', () => {
-  test('Should return error if field is error', () => {
+  test('Should return error if field is empty', () => {
     const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
