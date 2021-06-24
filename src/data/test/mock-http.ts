5b8d5a93a7cc5b2ca1e35d10f46f3d68f87458a8
Landersson Almeida <clandokca@gmail.com>
2021-06-24 16:29:23 -0300

    refactor: rename data mocks

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,10 @@
 import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '@/data/protocols/http'
+import faker from 'faker'
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.datatype.json()
+})
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,6 +1,6 @@
 import faker from 'faker'
 import { LocalSaveAccessToken } from './local-save-access-token'
-import { SetStorageMock } from '@/data/test/mock-storage'
+import { SetStorageMock } from '@/data/test'
 
 type SutTypes = {
   sut: LocalSaveAccessToken
