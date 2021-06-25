7f4692d7dd90382d381df31a2535566f7e3900d6
Landersson Almeida <clandokca@gmail.com>
2021-06-25 20:55:03 -0300

    test: ensure SignUp change name status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,33 +1,54 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import faker from 'faker'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-// import faker from 'faker'
 import SignUp from './signup'
-import { Helper } from '@/presentation/test'
+import { Helper, ValidationStub } from '@/presentation/test'
 
 // type SutTypes = {
 //   authenticationSpy: AuthenticationSpy
 //   saveAccessTokenMock: SaveAccessTokenMock
 // }
 
-const makeSut = (): void => {
+type SutParams = {
+  validationError: string
+}
+
+const makeSut = (params?: SutParams): void => {
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = params?.validationError
+
   render(
     // <Router history={history}>
-      <SignUp />
+      <SignUp validation={validationStub} />
     // </Router>
   )
 }
 
+const populateField = (fieldName: string, value = faker.random.word()): void => {
+  const input = screen.getByTestId(fieldName)
+  fireEvent.input(input, { target: { value } })
+}
+
 describe('SignUp Component', () => {
   test('Should start with initial state', () => {
-    const validationError = 'Campo obrigatório'
-    makeSut()
+    const validationError = faker.random.word()
+    makeSut({ validationError })
 
     Helper.testChildCount('error-wrap', 0)
     Helper.testButtonIsDisabled('submit', true)
     Helper.testStatusForField('name', validationError)
-    Helper.testStatusForField('email', validationError)
-    Helper.testStatusForField('password', validationError)
-    Helper.testStatusForField('passwordConfirmation', validationError)
+    Helper.testStatusForField('email', 'Campo obrigatório')
+    Helper.testStatusForField('password', 'Campo obrigatório')
+    Helper.testStatusForField('passwordConfirmation', 'Campo obrigatório')
+  })
+
+  test('Should show name error if Validation fails', () => {
+    const validationError = faker.lorem.words()
+
+    makeSut({ validationError })
+    populateField('name')
+
+    Helper.testStatusForField('name', validationError)
   })
 })
