6225a3fb9b8d5649a6510cbb53d378d645773198
Landersson Almeida <clandokca@gmail.com>
2021-06-25 20:27:34 -0300

    feat: ensure SignUp starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
 import Styles from './signup-styles.scss'
 import { LoginHeader, Input, FormStatus, Footer } from '@/presentation/components'
 import FormContext from '@/presentation/contexts/form/form-context'
 
+type StateProps = {
+  isLoading: boolean
+  nameError: string | null
+  emailError: string | null
+  passwordError: string | null
+  passwordConfirmationError: string | null
+  mainError: string
+}
+
 const SignUp: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    nameError: 'Campo obrigatório',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    passwordConfirmationError: 'Campo obrigatório',
+    mainError: ''
+  })
+
   return (
     <div className={Styles.signup}>
       <LoginHeader />
-      <FormContext.Provider value={{ state: {} }}>
+      <FormContext.Provider value={{ state }}>
         <form role="form" className={Styles.form}>
           <h2>Criar Conta</h2>
           <Input type="text" name="name" placeholder="Digite seu nome" />
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
-          <Input type="password" name="password_confirmation" placeholder="Repita sua senha" />
-          <button role="submit" className={Styles.submit} type="submit">Entrar</button>
-          <Link to="/login" className={Styles.link}>Voltar Para Login</Link>
+          <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
+          <button className={Styles.submit} disabled type="submit" role="submit">Entrar</button>
+          <span className={Styles.link}>Voltar Para Login</span>
           <FormStatus />
         </form>
       </FormContext.Provider>
