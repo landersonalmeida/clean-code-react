08df69c9f24b3ab1152cebefdbd5cee048818875
Landersson Almeida <clandokca@gmail.com>
2021-06-28 18:41:32 -0300

    test: ensure Login begins with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('login')
+  })
+
   it('Should load with correct initial state', () => {
-    cy.visit('/login')
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByRole('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
