224148b18dfd69349f32d4053f6418f1e4b5146d
Landersson Almeida <clandokca@gmail.com>
2021-06-28 18:40:56 -0300

    feat: add getByRole Cypress command

--- src/main/test/cypress/support/index.d.ts
+++ src/main/test/cypress/support/index.d.ts
@@ -1,5 +1,6 @@
 declare namespace Cypress {
   interface Chainable {
     getByTestId: (id: string) => Chainable<Element>
+    getByRole: (role: string) => Chainable<Element>
   }
 }
--- src/main/test/cypress/support/index.js
+++ src/main/test/cypress/support/index.js
@@ -1,1 +1,2 @@
 Cypress.Commands.add('getByTestId', (id) => cy.get(`[data-testid=${id}]`))
+Cypress.Commands.add('getByRole', (role) => cy.get(`[role=${role}]`))
