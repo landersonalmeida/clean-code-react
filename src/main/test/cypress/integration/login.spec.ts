94b2072c91085a6faabdcb6d998fe10a07a259ae
Landersson Almeida <clandokca@gmail.com>
2021-06-29 18:51:17 -0300

    refactor: change test to simulate submit with enter key

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -83,7 +83,7 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/login`)
   })
 
-  it('Should present UnexpectedError if invalid credentials are provided', () => {
+  it('Should present UnexpectedError if invalid data is returned', () => {
     cy.intercept('POST', /login/, {
       statusCode: 200,
       body: {
@@ -92,8 +92,7 @@ describe('Login', () => {
     })
 
     cy.getByTestId('email').focus().type(faker.internet.email())
-    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
-    cy.getByRole('submit').click()
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5)).type('{enter}')
     cy.getByTestId('error-wrap')
     cy.getByTestId('spinner').should('not.exist')
     cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu.')
