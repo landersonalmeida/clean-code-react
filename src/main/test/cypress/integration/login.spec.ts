a15b8744b848922961acbdd01dcf12e54e0b94aa
Landersson Almeida <clandokca@gmail.com>
2021-06-29 18:48:19 -0300

    test: prevent Login to call submit multiple times

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -116,4 +116,18 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/`)
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
+
+  it('Should prevent multiple submits', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid()
+      }
+    }).as('request')
+
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByRole('submit').dblclick()
+    cy.get('@request.all').should('have.length', 1)
+  })
 })
