cb4ce02f41a91a1e9e9508fb24ab23f8cd7545f7
Landersson Almeida <clandokca@gmail.com>
2021-08-08 21:12:01 -0300

    refactor: change localStorageAdapter to sync instead of async

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -9,19 +9,11 @@ describe('LocalStorageAdapter', () => {
     localStorage.clear()
   })
 
-  test('Should call localStorage with correct values', async () => {
+  test('Should call localStorage with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
-    await sut.set(key, value)
-    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
-  })
-
-  test('Should throw if LocalStorageAdapter throws', async () => {
-    const sut = makeSut()
-    const key = faker.database.column()
-    const value = faker.random.word()
-    await sut.set(key, value)
+    sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
   })
 })
