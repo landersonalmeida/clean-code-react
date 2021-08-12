440e9d763bf43841bf2d20f25445886e281deede
Landersson Almeida <clandokca@gmail.com>
2021-08-12 10:54:09 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
 import { SurveyList } from '..'
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
+import { SurveyModel } from '@/domain/models'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+
+  async loadAll(): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+
+  return { loadSurveyListSpy }
+}
 
 describe('SurveyList Component', () => {
   test('Should present 4 empty items on start', () => {
-    render(<SurveyList />)
+    makeSut()
 
     const surveyList = screen.getByTestId('survey-list')
 
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  test('Should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
