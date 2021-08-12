de720feb29613822943e08b185cf07494aae4415
Landersson Almeida <clandokca@gmail.com>
2021-08-12 11:26:56 -0300

    test: ensure SurveyItem load survey data correctly

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -1,7 +1,7 @@
 import faker from 'faker'
 import { SurveyModel } from '../models'
 
-export const mockSurveyListModel = (): SurveyModel[] => ([{
+export const mockSurveyModel = (): SurveyModel => ({
   id: faker.datatype.uuid(),
   question: faker.random.words(10),
   answers: [{
@@ -13,4 +13,10 @@ export const mockSurveyListModel = (): SurveyModel[] => ([{
   }],
   didAnswer: faker.datatype.boolean(),
   date: faker.date.recent()
-}])
+})
+
+export const mockSurveyListModel = (): SurveyModel[] => ([
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel()
+])
