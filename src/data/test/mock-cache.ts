24e5caf8232ac3d5fd0ec7976a9cc60093fd6fe0
Landersson Almeida <clandokca@gmail.com>
2021-08-14 15:57:27 -0300

    test: ensure AuthorizeHttpGetClientDecorator add token to HttpGetClient

--- src/data/test/mock-cache.ts
+++ src/data/test/mock-cache.ts
@@ -3,9 +3,10 @@ import { GetStorage } from '../protocols/cache'
 
 export class GetStorageSpy implements GetStorage {
   key?: string
-  value = faker.random.objectElement()
+  value: any = faker.random.objectElement()
 
   get(key: string): any {
     this.key = key
+    return this.value
   }
 }
