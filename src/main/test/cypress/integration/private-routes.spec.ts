c14743baa1cc32cfa51c5fe3abf2e764d068dcb1
Landersson Almeida <clandokca@gmail.com>
2021-08-30 18:09:06 -0300

    test: ensure SurveyList present error on UnexpectedError

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -1,7 +1,4 @@
-import * as FormHelper from '../support/form-helpers'
 import * as Helper from '../support/helpers'
-import * as Http from '../support/login-mocks'
-import faker from 'faker'
 
 describe('PrivateRoutes', () => {
   it('Should logout if survey-list has no token', () => {
---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,18 @@
+import * as Helper from '../support/helpers'
+import * as Http from '../support/survey-list-mocks'
+import faker from 'faker'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helper.setLocalStorageItem('account', {
+      accessToken: faker.datatype.uuid(),
+      name: faker.name.findName()
+    })
+    cy.visit('')
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu.')
+  })
+})
