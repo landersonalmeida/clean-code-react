d163cb1d376c32b65a2b93c915d085275e2c19a4
Landersson Almeida <clandokca@gmail.com>
2021-09-07 18:25:30 -0300

    fix: husky pre-push add cypress

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -16,6 +16,7 @@ describe('SurveyList', () => {
 
   it('Should present error on UnexpectedError', () => {
     mockUnexpectedError()
+    cy.wait('@request')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu.')
   })
 
