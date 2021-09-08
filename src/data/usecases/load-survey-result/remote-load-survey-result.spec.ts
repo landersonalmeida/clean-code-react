ede79b318dcc3aded57b68f95023bb8d82b37e39
Landersson Almeida <clandokca@gmail.com>
2021-09-08 18:02:57 -0300

    test: ensure RemoteLoadSurveyResult calls HttpGetClient with correct URL

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -0,0 +1,13 @@
+import { RemoteLoadSurveyResult } from '@/data/usecases'
+import { HttpGetClientSpy } from '@/data/test'
+import faker from 'faker'
+
+describe('RemoteLoadSurveyResult', () => {
+  test('Should call HttpGetClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const httpGetClientSpy = new HttpGetClientSpy()
+    const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+    await sut.load()
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+})
