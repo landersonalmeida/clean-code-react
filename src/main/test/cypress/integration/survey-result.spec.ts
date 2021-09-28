3b528f8315451e4170f3d39b5f390b8950daf168
Landersson Almeida <clandokca@gmail.com>
2021-09-28 16:35:27 -0300

    test: ensure SurveyResult backs to SurveyList on back button click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -52,4 +52,12 @@ describe('SurveyResult', () => {
       assert.notExists(li.find('[data-testid="image"]'))
     })
   })
+
+  it('Should go to SurveyList on back button click', () => {
+    cy.visit('')
+    mockSuccess()
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('back-button').click()
+    Helper.testUrl('/')
+  })
 })
