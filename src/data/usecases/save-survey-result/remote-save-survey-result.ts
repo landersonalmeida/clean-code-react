aee2bb4f67f36d1def81af98a6d6aea084d8f861
Landersson Almeida <clandokca@gmail.com>
2021-10-03 17:44:30 -0300

    feat: ensure RemoteSaveSurveyResult returns UnexpectedError on 404

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -1,6 +1,6 @@
 import { RemoteSurveyResultModel } from '@/data/models'
 import { HttpClient, HttpStatusCode } from '@/data/protocols/http'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { SaveSurveyResult } from '@/domain/usecases'
 
 export class RemoteSaveSurveyResult implements SaveSurveyResult {
@@ -16,8 +16,9 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
       body: params
     })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return null!
       case HttpStatusCode.forbidden: throw new AccessDeniedError()
-      default: return null!
+      default: throw new UnexpectedError()
     }
   }
 }
